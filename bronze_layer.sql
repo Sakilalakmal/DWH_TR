5628ac1de01906e3a34cf4735b4c0f40b2dcc523
Ganwari Nakathige Sakila Lakmal Nandasiri CB014383 <164737611+Sakilalakmal@users.noreply.github.com>
2026-02-24 12:28:48 +0530

    feat: add silver layer DWH SQL scripts

--- bronze_layer.sql
+++ bronze_layer.sql
@@ -16,6 +16,7 @@ IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
 CREATE TABLE bronze.crm_cust_info (
     cst_id                 INT,
     cst_key                NVARCHAR(50),
+    cst_firstname          NVARCHAR(50),
     cst_lastname           NVARCHAR(50),
     cst_material_status    NVARCHAR(50),
     cst_gender             NVARCHAR(50),
